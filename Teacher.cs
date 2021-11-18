54f76aa69472d839929d84de48cfd4ed713e00be
ThanhMai1220 <94424011+ThanhMai1220@users.noreply.github.com>
2021-11-18 09:57:41 +0700

    them file Teacher

---
+++ Teacher.cs
@@ -0,0 +1,45 @@
+namespace Hello
+{
+    public class Teacher
+    {
+    //     private string name;
+    //     private int age;
+    //    private string course;
+    //     public string  Name
+    //     {
+    //      get { return name; }
+    //      set { name = value; }
+    //     }
+    //     public int Age
+    //     {
+    //      get { return age;}
+    //      set{
+    //         if (value < 0) age = 0;
+    //         else value = age;
+    //       }
+    //      }
+    //     public string Course
+    // {
+    //         get{return course;}
+    //         set{ course = Course;}
+            
+    //     }
+    //     public Teacher(string name, int age, string course)
+    //      {
+    //     Name = name;
+    //     Age = age;
+    //     Course = course;
+    //      }
+        public void Show()
+        {
+            System.Console.WriteLine("Nothing yet");
+            // System.Console.WriteLine("Name: {" + Name + "}");
+            // System.Console.WriteLine("Age: {" + Age + "}");
+            // System.Console.WriteLine("Count: {" + Course + "}");
+        }
+
+    }
+    
+    
+
+}
